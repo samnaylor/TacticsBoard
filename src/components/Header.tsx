bb68ec0bdf800314b7b8f3806fdaba9f21db7ba6
Sam Naylor <samnaylor2255@gmail.com>
2026-09-02 09:07:51 +0100

    increase reset icon size

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -59,7 +59,7 @@ const Header = () => {
             }}
             className="rounded-lg px-2.5 py-2 text-xs font-semibold text-white/50 transition hover:bg-white/10 hover:text-white"
           >
-            <RiResetLeftFill />
+            <RiResetLeftFill className="w-6 h-6" />
           </button>
 
           <button
